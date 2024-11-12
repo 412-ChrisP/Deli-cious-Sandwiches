42f134d14dc7424d3370c7ddd9020322dca61a59
412-ChrisP <chrispbacon542@gmail.com>
2024-11-12 09:55:28 -0500

    added blank methods and attributes to following classes

--- src/main/java/YearUp/pluralsight/Sandwich.java
+++ src/main/java/YearUp/pluralsight/Sandwich.java
@@ -1,4 +1,32 @@
 package YearUp.pluralsight;
 
-public class Sandwich {
+import java.util.*;
+
+public class Sandwich
+{
+    private String size;
+    private String bread;
+    private boolean toasted;
+    private List<Toppings> toppings;
+
+    public Sandwich()
+    {
+
+    }
+
+    public void selectBread(String bread)
+    {
+
+    }
+
+    public void selectSize(String size)
+    {
+
+    }
+
+    public double calculateSandwichPrice()
+    {
+
+        return 0.0;
+    }
 }
--- src/main/java/YearUp/pluralsight/Userinterface.java
+++ src/main/java/YearUp/pluralsight/Userinterface.java
@@ -1,8 +1,15 @@
 package YearUp.pluralsight;
 
+import java.util.*;
+
 public class Userinterface
 {
     public static void main(String[] args)
+    {
+
+    }
+
+    public void startNewOrder()
     {
 
     }
