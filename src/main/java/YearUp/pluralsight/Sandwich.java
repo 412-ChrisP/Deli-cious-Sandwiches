8afa244ff8ab9386a1b9d6bd589be6c377fdf3e7
412-ChrisP <chrispbacon542@gmail.com>
2024-11-13 14:16:51 -0500

    added addToppings method and calcSandwichPrice along with other minor methods

--- src/main/java/YearUp/pluralsight/Sandwich.java
+++ src/main/java/YearUp/pluralsight/Sandwich.java
@@ -7,26 +7,67 @@ public class Sandwich
     private String size;
     private String bread;
     private boolean toasted;
-    private List<Toppings> toppings;
+    private final List<Toppings> toppings = new ArrayList<>();
 
     public Sandwich()
-    {
-
-    }
+    {}
 
     public void selectBread(String bread)
     {
-
+        this.bread = bread;
     }
 
     public void selectSize(String size)
     {
-
+        switch (size)
+        {
+            case "4" -> this.size = String.valueOf(SandwichSize.FOUR_INCH);
+            case "8" -> this.size = String.valueOf(SandwichSize.EIGHT_INCH);
+            case "12" -> this.size = String.valueOf(SandwichSize.TWELVE_INCH);
+            default -> throw new IllegalArgumentException("Invalid sandwich size: " + size);
+        }
     }
 
     public double calculateSandwichPrice()
     {
+        double totalPrice = 0.0;
+        SandwichSize sandwichSize = SandwichSize.valueOf(size.toUpperCase());
 
-        return 0.0;
+        for (Toppings topping : toppings)
+        {
+            totalPrice += topping.calcToppingPrice(sandwichSize);
+        }
+
+        return totalPrice;
+    }
+
+    public void addToppings()
+    {
+        Toppings toppingsSelection = new Toppings()
+        {
+            @Override
+            public double calcToppingPrice(SandwichSize size)
+            {
+                return 0;
+            }
+        };
+
+        toppingsSelection.getToppingType();
+    }
+
+    public void setToasted(boolean toasted)
+    {
+        this.toasted = toasted;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Sandwich{" +
+                "size:'" + size + '\'' +
+                ", bread:'" + bread + '\'' +
+                ", toasted:" + toasted +
+                ", toppings:" + toppings +
+                '}';
     }
 }
