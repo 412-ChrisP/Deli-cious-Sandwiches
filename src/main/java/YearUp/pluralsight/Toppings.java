fc1c08b33405336b487ecc20458d962ea7b1d8d2
412-ChrisP <chrispbacon542@gmail.com>
2024-11-13 14:01:03 -0500

    added methods and adjusted minor details

--- src/main/java/YearUp/pluralsight/Toppings.java
+++ src/main/java/YearUp/pluralsight/Toppings.java
@@ -1,12 +1,13 @@
 package YearUp.pluralsight;
 
+import java.util.*;
+
 public abstract class Toppings
 {
     protected String name;
 
-    public Toppings(String name)
+    public Toppings()
     {
-        this.name = name;
     }
 
     public String getName()
@@ -14,9 +15,33 @@ public abstract class Toppings
         return name;
     }
 
-    public void toppingType()
+    public void getToppingType()
     {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        boolean addingToppings = true;
+        while (addingToppings)
+        {
+            System.out.println("Select a topping category:");
+            System.out.println("1) Meat");
+            System.out.println("2) Cheese");
+            System.out.println("3) Regular Toppings");
+            System.out.println("4) Sauces");
+            System.out.println("0) Done adding toppings");
+            System.out.print("Choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice)
+            {
+                case 1 -> MeatToppings.getMeatTopping();
+                case 2 -> CheeseToppings.getCheeseTopping();
+                case 3 -> RegularToppings.getRegularTopping();
+                case 4 -> SauceToppings.getSauceTopping();
+                case 0 -> addingToppings = false;
+                default -> System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 
     public abstract double calcToppingPrice(SandwichSize size);
