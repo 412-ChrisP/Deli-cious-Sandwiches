3559d46d29a9aee74c00be717311a686b700cd0c
412-ChrisP <chrispbacon542@gmail.com>
2024-11-14 15:54:13 -0500

    added ToppingsTest class with test methods

--- src/main/java/YearUp/pluralsight/ToppingsTest.java
+++ src/main/java/YearUp/pluralsight/ToppingsTest.java
@@ -1,4 +1,49 @@
 package YearUp.pluralsight;
 
-public class ToppingsTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ToppingsTest
+{
+    @Test
+    public void meatTopping_calcToppingPrice_shouldReturnCorrectPrice()
+    {
+        // Arrange
+        Toppings meatTopping = new MeatToppings("steak");
+        double expectedPrice = 2.00;
+
+        // Act
+        double actualPrice = meatTopping.calcToppingPrice(SandwichSize.EIGHT_INCH);
+
+        // Assert
+        assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Test
+    public void cheeseTopping_calcToppingPrice_shouldReturnCorrectPrice()
+    {
+        // Arrange
+        Toppings cheeseTopping = new CheeseToppings("cheddar");
+        double expectedPrice = 1.50;
+
+        // Act
+        double actualPrice = cheeseTopping.calcToppingPrice(SandwichSize.EIGHT_INCH);
+
+        // Assert
+        assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Test
+    public void sauceTopping_calcToppingPrice_shouldReturnZero()
+    {
+        // Arrange
+        Toppings sauceTopping = new SauceToppings("ranch");
+        double expectedPrice = 0.00;
+
+        // Act
+        double actualPrice = sauceTopping.calcToppingPrice(SandwichSize.EIGHT_INCH);
+
+        // Assert
+        assertEquals(expectedPrice, actualPrice);
+    }
 }
